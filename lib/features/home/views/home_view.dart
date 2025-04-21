84eecab8c9546ae3e3bd34d967232e4fd4d13170
Anna Sidor <annasidor00@gmail.com>
2025-04-22 00:58:36 +0200

    feat: create dio client and api providers

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "../../../app/app.dart";
+import "../../route_map/repository/route_map_repository.dart";
 
 class MyHomeView extends StatefulWidget {
   const MyHomeView({super.key, required this.title});
@@ -32,6 +34,7 @@ class _MyHomeViewState extends State<MyHomeView> {
             Text("$_counter", style: Theme.of(context).textTheme.headlineMedium),
             // temporary
             OutlinedButton(onPressed: () async => context.router.pushRouteMap(1), child: const Text("Route Map")),
+            const TestProviderWigdet(),
           ],
         ),
       ),
@@ -40,6 +43,32 @@ class _MyHomeViewState extends State<MyHomeView> {
         tooltip: "Increment",
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class TestProviderWigdet extends ConsumerWidget {
+  const TestProviderWigdet({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final routes = ref.watch(fetchAllRoutesProvider);
+    final route_2 = ref.watch(fetchRouteWithIdProvider(2));
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        routes.when(
+          data: (routes) => Text("Routes: $routes"),
+          loading: CircularProgressIndicator.new,
+          error: (e, _) => Text("Error: $e"),
+        ),
+        route_2.when(
+          data: (route) => Text("Route 2: $route"),
+          loading: CircularProgressIndicator.new,
+          error: (e, _) => Text("Error: $e"),
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/route_map/repository/route_map_repository.dart
@@ -0,0 +1,35 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:riverpod_annotation/riverpod_annotation.dart";
+
+import "../../../app/config/env.dart";
+import "../../../common/data_source/remote/dio_client.dart";
+
+part "route_map_repository.g.dart";
+
+@riverpod
+Future<Map<String, dynamic>> fetchAllRoutes(Ref ref) async {
+  final dio = ref.watch(dioClientProvider);
+  final response = await dio.get<Map<String, dynamic>>(
+    "${Env.apiUrl}items/routes?fields=*,landmarks.landmarks_id.*,landmarks.landmarks_id.location.*",
+  );
+
+  if (response.statusCode == 200) {
+    return response.data!;
+  } else {
+    throw Exception("Failed to load users");
+  }
+}
+
+@riverpod
+Future<Map<String, dynamic>> fetchRouteWithId(Ref ref, int id) async {
+  final dio = ref.watch(dioClientProvider);
+  final response = await dio.get<Map<String, dynamic>>(
+    "${Env.apiUrl}items/routes?fields=*,landmarks.landmarks_id.*,landmarks.landmarks_id.location.*&filter[id][_eq]=$id",
+  );
+
+  if (response.statusCode == 200) {
+    return response.data!;
+  } else {
+    throw Exception("Failed to load users");
+  }
+}
