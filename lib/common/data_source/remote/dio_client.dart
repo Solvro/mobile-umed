20806e400bd8b0ff6c3a9cb1d407863b7b0e786a
Anna Sidor <annasidor00@gmail.com>
2025-04-22 23:11:35 +0200

    refactor: move baseUrl to dio settings

--- lib/common/data_source/remote/dio_client.dart
+++ lib/common/data_source/remote/dio_client.dart
@@ -2,9 +2,17 @@ import "package:dio/dio.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
+import "../../../app/config/env.dart";
+
 part "dio_client.g.dart";
 
 @Riverpod(keepAlive: true)
 Dio dioClient(Ref ref) {
-  return Dio();
+  final options = BaseOptions(
+    baseUrl: Env.apiUrl,
+    connectTimeout: const Duration(seconds: 5),
+    receiveTimeout: const Duration(seconds: 3),
+  );
+
+  return Dio(options);
 }
--- lib/features/route_map/repository/route_map_repository.dart
+++ lib/features/route_map/repository/route_map_repository.dart
@@ -1,7 +1,6 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
-import "../../../app/config/env.dart";
 import "../../../common/data_source/remote/dio_client.dart";
 
 part "route_map_repository.g.dart";
@@ -10,7 +9,7 @@ part "route_map_repository.g.dart";
 Future<Map<String, dynamic>> fetchAllRoutes(Ref ref) async {
   final dio = ref.watch(dioClientProvider);
   final response = await dio.get<Map<String, dynamic>>(
-    "${Env.apiUrl}items/routes?fields=*,landmarks.landmarks_id.*,landmarks.landmarks_id.location.*",
+    "items/routes?fields=*,landmarks.landmarks_id.*,landmarks.landmarks_id.location.*",
   );
 
   if (response.statusCode == 200) {
@@ -24,7 +23,7 @@ Future<Map<String, dynamic>> fetchAllRoutes(Ref ref) async {
 Future<Map<String, dynamic>> fetchRouteWithId(Ref ref, int id) async {
   final dio = ref.watch(dioClientProvider);
   final response = await dio.get<Map<String, dynamic>>(
-    "${Env.apiUrl}items/routes?fields=*,landmarks.landmarks_id.*,landmarks.landmarks_id.location.*&filter[id][_eq]=$id",
+    "items/routes?fields=*,landmarks.landmarks_id.*,landmarks.landmarks_id.location.*&filter[id][_eq]=$id",
   );
 
   if (response.statusCode == 200) {
