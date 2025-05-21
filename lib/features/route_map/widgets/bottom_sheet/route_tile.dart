1c028d43aaa2723bf9e3e51ea2bb4b619818f6b6
24bartixx <jestgitbartix@gmail.com>
2025-05-21 20:21:14 +0200

    feat: add playlist expandable section in select route bottom sheet

--- lib/features/route_map/widgets/bottom_sheet/route_tile.dart
+++ lib/features/route_map/widgets/bottom_sheet/route_tile.dart
@@ -1,13 +1,31 @@
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "../../../../app/config/ui_config.dart";
 import "../../../../app/l10n/l10n.dart";
 import "../../../../app/theme/app_theme.dart";
+import "../../../../common/data_source/mocks/mock_songs.dart";
 import "../../../../common/models/route.dart" as rt;
+import "../../../../common/widgets/secondary_action_button.dart";
+import "playlist_info_section.dart";
 import "route_stat.dart";
 
-class RouteTile extends StatelessWidget {
+class RouteTile extends ConsumerStatefulWidget {
   const RouteTile({super.key, required this.route});
 
   final rt.Route route;
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => RouteTileState();
+}
+
+class RouteTileState extends ConsumerState<RouteTile> {
+  late RouteInfoOption _chosenOption;
+
+  @override
+  void initState() {
+    super.initState();
+    _chosenOption = RouteInfoOption.info;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +36,51 @@ class RouteTile extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(route.name, style: context.textTheme.headlineSmall?.copyWith(fontSize: 22)),
-                const SizedBox(height: 8),
-                Text(route.distance.inKilometers()),
+                Text(widget.route.name, style: context.textTheme.headlineSmall?.copyWith(fontSize: 22)),
+                const SizedBox(height: 6),
+                Text(widget.route.distance.inKilometers()),
               ],
             ),
           ),
           RouteStat(
             imgPath: "assets/images/water.png",
-            comment: route.requiredWater?.inMililiters() ?? context.l10n.ml_0,
+            comment: widget.route.requiredWater?.inMililiters() ?? context.l10n.ml_0,
           ),
-          RouteStat(imgPath: "assets/images/time.png", comment: route.estimatedTime.inMinutes()),
-          RouteStat(imgPath: "assets/images/kcal.png", comment: route.calories?.inKcal() ?? context.l10n.kcal_0),
+          RouteStat(imgPath: "assets/images/time.png", comment: widget.route.estimatedTime.inMinutes()),
+          RouteStat(imgPath: "assets/images/kcal.png", comment: widget.route.calories?.inKcal() ?? context.l10n.kcal_0),
         ],
       ),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(6),
+          child: Row(
+            spacing: BottomSheetHeaderConfig.controlsSpacing,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SecondaryActionButton(
+                onPressed: () {
+                  setState(() {
+                    _chosenOption = RouteInfoOption.info;
+                  });
+                },
+                text: context.l10n.route_description,
+              ),
+
+              SecondaryActionButton(
+                onPressed: () {
+                  setState(() {
+                    _chosenOption = RouteInfoOption.playlist;
+                  });
+                },
+                text: context.l10n.playlist,
+              ),
+            ],
+          ),
+        ),
+        if (_chosenOption == RouteInfoOption.info) const Text("fdsfsd") else PlaylistInfoSection(songs: mockSongs),
+      ],
     );
   }
 }
+
+enum RouteInfoOption { info, playlist }
