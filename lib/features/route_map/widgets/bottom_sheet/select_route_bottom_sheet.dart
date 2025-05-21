4d82a17a6391cbb143d903dc9696b4304a1e4ecb
24bartixx <jestgitbartix@gmail.com>
2025-05-21 17:52:14 +0200

    fix: change select route bottom sheet to extended

--- lib/features/route_map/widgets/bottom_sheet/select_route_bottom_sheet.dart
+++ lib/features/route_map/widgets/bottom_sheet/select_route_bottom_sheet.dart
@@ -5,6 +5,7 @@ import "../../../../app/config/ui_config.dart";
 import "../../../../app/l10n/l10n.dart";
 import "../../../../app/theme/color_consts.dart";
 import "../../../../common/data_source/mocks/mock_routes.dart";
+import "../../../../common/providers/bottom_sheet_providers.dart";
 import "../../../../common/widgets/main_action_button.dart";
 import "../../../../common/widgets/map_bottom_sheet.dart";
 import "route_tile.dart";
@@ -15,6 +16,14 @@ class ChooseRouteBottomSheet extends ConsumerStatefulWidget {
 }
 
 class ChooseRouteBottomSheetState extends ConsumerState<ChooseRouteBottomSheet> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      ref.read(sheetModeProvider.notifier).state = SheetMode.expanded;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MapBottomSheet(
