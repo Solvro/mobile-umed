c78201704f9b0d4d23f150c8acd5990568d385cc
BombardierBulge <z.bartosz.z@wp.pl>
2025-05-24 15:38:49 +0200

    fix: delete useless files

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,10 @@ import "package:flutter_map_tile_caching/flutter_map_tile_caching.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "app/app.dart";
-import "common/config/hive_init.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await initializeHive();
   await FMTCObjectBoxBackend().initialise();
   runApp(const ProviderScope(child: MyApp()));
 }
