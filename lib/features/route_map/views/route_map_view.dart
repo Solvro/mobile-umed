0fb8f8150a3d7811f7a587eded8726f0651707e2
BombardierBulge <z.bartosz.z@wp.pl>
2025-05-24 14:55:14 +0200

    feat: Add caching for map data

--- lib/features/route_map/views/route_map_view.dart
+++ lib/features/route_map/views/route_map_view.dart
@@ -1,13 +1,16 @@
 import "package:flutter/material.dart" hide Route;
 import "package:flutter_map/flutter_map.dart";
+import "package:flutter_map_tile_caching/flutter_map_tile_caching.dart";
+
 import "../../../app/app.dart";
 import "../../../app/config/flutter_map_config.dart";
 import "../../../common/models/route.dart";
 
 class RouteMapView extends StatelessWidget {
-  const RouteMapView({super.key, required this.route});
+  RouteMapView({super.key, required this.route});
 
   final Route route;
+  final _tileProvider = FMTCTileProvider(stores: const {"mapStore": BrowseStoreStrategy.readUpdateCreate});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,7 @@ class RouteMapView extends StatelessWidget {
           FlutterMap(
             options: MapOptions(initialCenter: route.landmarks.first.location),
             children: [
-              TileLayer(urlTemplate: FlutterMapConfig.urlTemplate, maxZoom: 19),
+              TileLayer(urlTemplate: FlutterMapConfig.urlTemplate, tileProvider: _tileProvider, maxZoom: 19),
               PolylineLayer(
                 polylines: [
                   Polyline(
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter_map_tile_caching/flutter_map_tile_caching.dart";
+
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "app/app.dart";
@@ -8,6 +10,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeHive();
-
+  await FMTCObjectBoxBackend().initialise();
   runApp(const ProviderScope(child: MyApp()));
 }
