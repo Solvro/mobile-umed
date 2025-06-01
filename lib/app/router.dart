e7a153dbb1de2a5b162e2af0d27e61e5b0ff80d5
szymonprotynski <protynski.szym2on@gmail.com>
2025-06-01 12:15:41 +0200

    feat: add profile page and routing

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -2,7 +2,7 @@ part of "app.dart";
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 final _shellNavigatorHomeKey = GlobalKey<NavigatorState>();
-final _shellNavigatorAchievementsKey = GlobalKey<NavigatorState>();
+final _shellNavigatorProfileKey = GlobalKey<NavigatorState>();
 final _shellNavigatorOthersKey = GlobalKey<NavigatorState>();
 
 final _router = GoRouter(
@@ -26,12 +26,12 @@ final _router = GoRouter(
           routes: [GoRoute(path: HomePage.routeName, builder: (context, state) => const HomePage())],
         ),
         StatefulShellBranch(
-          navigatorKey: _shellNavigatorAchievementsKey,
-          routes: [GoRoute(path: AchievementsPage.routeName, builder: (context, state) => const _NotImplementedPage())],
-        ),
-        StatefulShellBranch(
           navigatorKey: _shellNavigatorOthersKey,
           routes: [GoRoute(path: OthersPage.routeName, builder: (context, state) => const _NotImplementedPage())],
+        ),
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorProfileKey,
+          routes: [GoRoute(path: ProfilePage.routeName, builder: (context, state) => const ProfilePage())],
         ),
       ],
     ),
