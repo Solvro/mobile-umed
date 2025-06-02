544c26add4f2d6a3b46b21b7230837dff53f9486
szymonprotynski <protynski.szym2on@gmail.com>
2025-06-02 22:00:00 +0200

    feat: add mockAchievements

--- lib/common/data_source/mocks/mock_stats.dart
+++ lib/common/data_source/mocks/mock_stats.dart
@@ -1,18 +1,17 @@
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/material.dart";
-import "../../../../app/config/ui_config.dart";
-import "../../widgets/profile/horizontal_stat_card_list/stat_card.dart";
+import "../../widgets/profile/horizontal_stat_card_list/card.dart";
 
 final IList<StatCard> mockStats =
     [
-      const StatCard(
-        value: "10.5 km",
-        icon: Icon(Icons.directions_walk, color: Colors.green, size: StatListConfig.iconSize),
-      ),
-      const StatCard(
-        value: "500 kcal",
-        icon: Icon(Icons.local_fire_department, color: Colors.red, size: StatListConfig.iconSize),
-      ),
-      const StatCard(value: "2 L", icon: Icon(Icons.water_drop, color: Colors.blue, size: StatListConfig.iconSize)),
-      const StatCard(value: "3 h", icon: Icon(Icons.schedule, color: Colors.orange, size: StatListConfig.iconSize)),
+      StatCard(value: "10.5 km", iconCode: Icons.directions_walk.codePoint, iconColor: Colors.green.toARGB32()),
+      StatCard(value: "500 kcal", iconCode: Icons.local_fire_department.codePoint, iconColor: Colors.red.toARGB32()),
+      StatCard(value: "2 L", iconCode: Icons.water_drop.codePoint, iconColor: Colors.blue.toARGB32()),
+      StatCard(value: "3 h", iconCode: Icons.schedule.codePoint, iconColor: Colors.orange.toARGB32()),
+    ].lock;
+
+final IList<StatCard> mockAchievements =
+    [
+      StatCard(value: "Turysta", iconCode: Icons.local_see.codePoint, iconColor: Colors.black.toARGB32()),
+      StatCard(value: "Sigma", iconCode: Icons.man_3.codePoint, iconColor: Colors.black.toARGB32()),
     ].lock;
