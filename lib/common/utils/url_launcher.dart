8a3ec2e51a875a6737a9804b103eebb9d04cf2fb
Tomasz Trela <153635094+tomasz-trela@users.noreply.github.com>
2025-07-24 13:42:57 +0200

    fix: url launcher

--- lib/common/utils/url_launcher.dart
+++ lib/common/utils/url_launcher.dart
@@ -3,6 +3,6 @@ import "package:url_launcher/url_launcher.dart";
 Future<void> customLaunchUrl(String url) async {
   final uri = Uri.parse(url);
   if (await canLaunchUrl(uri)) {
-    await launchUrl(uri);
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
   }
 }
