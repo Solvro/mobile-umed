c05e28654c6a9f9a6e11e406d1caa8f661a8be12
24bartixx <jestgitbartix@gmail.com>
2025-08-03 19:57:00 +0200

    feat: shared preferences with cache num

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,26 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:hive/hive.dart";
 import "package:path_provider/path_provider.dart";
 import "package:sentry_flutter/sentry_flutter.dart";
-
+import "package:shared_preferences/shared_preferences.dart";
 import "app/app.dart";
 import "app/config/env.dart";
 import "app/wiredash.dart";
+import "common/providers/storage_service_provider.dart";
+import "common/utils/location_service.dart";
+import "common/utils/storage_service.dart";
+import "features/route_map/services/task_handlers/foreground_task_service.dart";
 
 void main() async {
   final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   FlutterForegroundTask.initCommunicationPort();
+
   final dir = await getApplicationDocumentsDirectory();
   Hive.init(dir.path);
+
+  await _requestPermissions();
+
   if (!kReleaseMode) {
     runMyApp();
   } else {
@@ -28,7 +36,21 @@ void main() async {
       options.tracesSampleRate = 0;
     }, appRunner: runMyApp);
   }
-  FlutterNativeSplash.remove();
 }
 
-void runMyApp() => runApp(const ProviderScope(child: ProdWiredash(child: WithForegroundTask(child: MyApp()))));
+Future<void> _requestPermissions() async {
+  await LocationService.requestPermissions();
+  await MyFlutterForegroundTask.requestPermissions();
+}
+
+Future<void> runMyApp() async {
+  final prefs = await SharedPreferences.getInstance();
+  final storage = StorageService(prefs);
+
+  runApp(
+    ProviderScope(
+      overrides: [storageServiceProvider.overrideWithValue(storage)],
+      child: const ProdWiredash(child: WithForegroundTask(child: MyApp())),
+    ),
+  );
+}
