752c11704f6a5c9402760868e12aabc96cd24471
BombardierBulge <z.bartosz.z@wp.pl>
2025-08-03 18:06:55 +0200

    feat: add splash screen gradient as image for -12 android and const color for android 12+ with flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flutter_foreground_task/flutter_foreground_task.dart";
+import "package:flutter_native_splash/flutter_native_splash.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:hive/hive.dart";
 import "package:path_provider/path_provider.dart";
 import "package:sentry_flutter/sentry_flutter.dart";
+
 import "app/app.dart";
 import "app/config/env.dart";
 import "app/wiredash.dart";
@@ -12,13 +14,14 @@ import "common/utils/location_service.dart";
 import "features/route_map/services/task_handlers/foreground_task_service.dart";
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   FlutterForegroundTask.initCommunicationPort();
   final dir = await getApplicationDocumentsDirectory();
   Hive.init(dir.path);
   await _requestPermissions();
   if (!kReleaseMode) {
-    return runMyApp();
+    runMyApp();
   } else {
     await SentryFlutter.init((options) {
       options.dsn = Env.bugsinkDsn;
@@ -26,6 +29,7 @@ void main() async {
       options.tracesSampleRate = 0;
     }, appRunner: runMyApp);
   }
+  FlutterNativeSplash.remove();
 }
 
 Future<void> _requestPermissions() async {
