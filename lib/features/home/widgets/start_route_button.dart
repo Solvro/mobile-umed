c9d2433e4058dc19359ac2f3d5042a1cfb53b926
Anna Sidor <annasidor00@gmail.com>
2025-08-19 10:35:19 +0200

    fix: remove leftover consumer widget

--- lib/features/home/widgets/start_route_button.dart
+++ lib/features/home/widgets/start_route_button.dart
@@ -1,14 +1,13 @@
 import "package:flutter/material.dart";
-import "package:flutter_riverpod/flutter_riverpod.dart";
 import "../../../../app/config/ui_config.dart";
 import "../../../../app/l10n/l10n.dart";
 import "../../../../app/theme/app_theme.dart";
 import "../../../app/app.dart";
 import "../../../common/widgets/styling/button_styles.dart";
 
-class StartRouteButton extends ConsumerWidget {
+class StartRouteButton extends StatelessWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: AppPaddings.medium),
       child: ElevatedButton(
